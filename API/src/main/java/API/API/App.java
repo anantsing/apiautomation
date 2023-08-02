8076aa8df75bea5d0cead84bdc5de52fc35fa51f
SCOREME-LT-ANANT <anantbhadoria99@gmail.com>
2023-08-02 23:52:38 +0530

    first commit

---
+++ API/src/main/java/API/API/App.java
@@ -0,0 +1,20 @@
+package API.API;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("skjdbcj");
+    App ap =new App();
+    ap.m1();
+    
+  }
+  public static void  m1() {
+	  System.out.println("kjsndckjn");
+  }
+  public static void  m11() {
+	  System.out.println("@@@@@@@@@kdckjn");
+  }
+  public static void  m12() {
+	  System.out.println("@@@@@@@@@kdckjn");
+  }
+}
